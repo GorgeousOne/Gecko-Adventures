edf8f40a63d023a688909d056a2df33e7e580479
flor2468 <jenny.doering@uni-weimar.de>
2022-04-30 16:36:57 +0200

    created/edited protoype scene

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -8,7 +8,7 @@ public class CameraFollow : MonoBehaviour {
 	public Transform target;
 
 	private void LateUpdate() {
-		Vector3 playerPos = new Vector3(target.position.x, 0, -10);
+		Vector3 playerPos = new Vector3(target.position.x, target.position.y, -10);
 		Vector3 lerpedPos = Vector3.Lerp(transform.position, playerPos, smoothSpeed);
 		transform.position = lerpedPos;
 	}

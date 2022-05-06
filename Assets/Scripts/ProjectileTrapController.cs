7c21d1b128018fb1cdd079fcf723f00dac25a7c3
IEatSushi <2klaus.4peter@gmail.com>
2022-05-06 13:18:54 +0200

    make arrows stick in objects

--- Assets/Scripts/ProjectileTrapController.cs
+++ Assets/Scripts/ProjectileTrapController.cs
@@ -1,28 +1,21 @@
-using System;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public class ArrowTrapController : MonoBehaviour {
+public class ProjectileTrapController : MonoBehaviour {
 
 	[FormerlySerializedAs("arrowCount")] [SerializeField] private int projectileCount = 1;
 	[SerializeField] private GameObject projectileType;
 	[SerializeField] private float projectileSpeed = 30;
-
-	private SpriteRenderer _renderer;
-	
-	private void Start() {
-		_renderer = GetComponent<SpriteRenderer>();
-	}
 
 	public void Shoot() {
 		if (projectileCount > 0) {
 			GameObject projectile = Instantiate(projectileType, transform.position, Quaternion.identity);
 			Quaternion shootRotation = transform.rotation;
 
-			if (_renderer.flipX) {
+			if (transform.localScale.x < 0) {
 				shootRotation *= Quaternion.Euler(0, 0, 180);
 			}
-			projectile.transform.rotation = shootRotation;
+			// projectile.transform.rotation = shootRotation;
 			projectile.GetComponent<Rigidbody2D>().velocity = shootRotation * Vector2.right * projectileSpeed;
 			projectileCount -= 1;
 		}

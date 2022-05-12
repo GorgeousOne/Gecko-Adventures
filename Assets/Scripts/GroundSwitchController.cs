b10054eec0ac466143747355f1ad41dde8745060
flor2468 <jenny.doering@uni-weimar.de>
2022-05-12 16:21:27 +0200

    changed grouns switch control to control to hit by player

--- Assets/Scripts/GroundSwitchController.cs
+++ Assets/Scripts/GroundSwitchController.cs
@@ -1,45 +1,52 @@
 using UnityEngine;
 using UnityEngine.Events;
 
-public class GroundSwitchController : Interactable {
+public class GroundSwitchController : MonoBehaviour {
 
     [SerializeField] private Vector2 openOffset;
 	[SerializeField] private float moveTime;
     
-    private SpriteRenderer _renderer;
-
     private bool _isEnabled;
-    private bool enableMovement = false;
-    
     private Vector2 _startPos;
-    
     private float _moveStart;
-    
     public SwitchToggleEvent toggleAction;
 
     // Start is called before the first frame update
     private void Start() {
-        _renderer = GetComponent<SpriteRenderer>();
-		interactAction.AddListener(ToggleSwitch);
+		//interactAction.AddListener(ToggleSwitch);
         _startPos = transform.position;
     }
 
-    public void ToggleSwitch() {
-        enableMovement = !enableMovement;
+    public void Update() {
         Vector2 startPos = _startPos;
         Vector2 targetPos = _startPos;
 
-        if (!enableMovement) {
+        if (_isEnabled) {
 			targetPos += openOffset;
 		}
 		else {
 			startPos += openOffset;
 		}
 
-		float elapsedTime = Mathf.Clamp(Time.time - _moveStart, 0, moveTime);
-		transform.position = Vector2.Lerp(startPos, targetPos, 0.01f);
+        float elapsedTime = Mathf.Clamp(Time.time - _moveStart, 0, moveTime);
+		transform.position = Vector2.Lerp(startPos, targetPos, elapsedTime / moveTime);
         
-        _isEnabled = enableMovement;
-		toggleAction.Invoke(_isEnabled);
+		//toggleAction.Invoke(_isEnabled);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.gameObject.layer == LayerMask.NameToLayer("Player")) {
+            collider.transform.parent = transform;
+            _isEnabled = true;
+            toggleAction.Invoke(_isEnabled);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider) {
+        if (collider.gameObject.layer == LayerMask.NameToLayer("Player")) {
+            collider.transform.parent = null;
+            _isEnabled = false;
+            toggleAction.Invoke(_isEnabled);
+        }
     }
 }

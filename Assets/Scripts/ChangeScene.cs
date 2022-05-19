75d2dd604021c963a52f136e63f3ea5710c7800a
flor2468 <jenny.doering@uni-weimar.de>
2022-05-19 16:53:39 +0200

    added PlatformControllerOld with old script & minor changes in some prefabs like FloorSwitch & added first version of level 1

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -4,8 +4,35 @@ using UnityEngine.SceneManagement;
 public class ChangeScene : Interactable {
 
     [SerializeField] private string sceneName;
+    // [SerializeField] private int currentSceneNumber;
+    // [SerializeField] private int sceneNumberToLoad;
+    // private Vector3 positionPreviousScene;
+    // [SerializeField] private GameObject player;
+
+    // void Awake() {
+    //     DontDestroyOnLoad(player);
+    // }
 
     protected override void OnInteract() {
+
+        // positionPreviousScene = transform.position;
+        // DontDestroyOnLoad(this.player);
         SceneManager.LoadScene(sceneName);
+
+        // if (currentSceneNumber < sceneNumberToLoad) {
+        //     LoadNextScene()
+        // }
+        // else {
+        //     LoadPreviousScene()
+        // }
+        
     }
+
+    // private void LoadNextScene() {
+    //     SceneManager.LoadScene(sceneName);
+    // }
+
+    // private void LoadPreviousScene() {
+        
+    // }
 }

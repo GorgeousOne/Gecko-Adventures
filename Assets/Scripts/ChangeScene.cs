41307e0bb30c37377fd1db003282fe8a8964159c
IEatSushi <2klaus.4peter@gmail.com>
2022-05-22 17:40:22 +0200

    move player position loading to PlayerSpawning script

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -6,7 +6,10 @@ public class ChangeScene : Interactable {
     [SerializeField] private string sceneName;
 
     protected override void OnInteract() {
-        
+        string currentSceneName = SceneManager.GetActiveScene().name;
+        PlayerPrefs.SetFloat("Player_X_" + currentSceneName, transform.position.x);
+        PlayerPrefs.SetFloat("Player_Y_" + currentSceneName, transform.position.y);
+        PlayerPrefs.SetFloat("Player_Z_" + currentSceneName, transform.position.z);
         SceneManager.LoadScene(sceneName);
     }
 }

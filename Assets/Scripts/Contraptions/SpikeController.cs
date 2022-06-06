b6813fbb4789f5c50b3760b7e55d0d22c080abf4
IEatSushi <2klaus.4peter@gmail.com>
2022-06-06 17:34:28 +0200

    add trample activated spikes

--- Assets/Scripts/Contraptions/SpikeController.cs
+++ Assets/Scripts/Contraptions/SpikeController.cs
@@ -1,36 +1,38 @@
-using System;
 using System.Collections;
 using UnityEngine;
 
 public class SpikeController : Resettable {
 	
 	[Header("Sprites")]
+	[SerializeField] private Collider2D damageCollider;
 	[SerializeField] private Sprite retracted;
 	[SerializeField] private Sprite extended;
 
 	[Header("Timing")] 
-	[SerializeField] private bool timedActivationEnabled = false;
+	[SerializeField] private bool timedActivationEnabled;
 	[SerializeField] [Min(0)] private float extendOffset;
 	[SerializeField] [Min(.5f)] private float extendTime = 2;
 	[SerializeField] [Min(.5f)] private float retractTime = 2;
-	
+
+	[Header("Trample Triggering")]
+	[SerializeField] private bool trampleActivationEnabled;
+	[SerializeField] private float trampleTriggerOffset = 1f;
+	[SerializeField] [Min(.5f)] private float trampleTriggerExtendTime = 4;
+
 	private bool _isExtended = true;
 	private SpriteRenderer _renderer;
-	private Collider2D _collider;
 
 	private bool _savedWasExtended;
 	
 	private void OnEnable() {
 		_renderer = GetComponent<SpriteRenderer>();
-		_collider = GetComponent<Collider2D>();
-		SetExtended(_isExtended);
+		SetExtended(_isExtended && !trampleActivationEnabled);
 	}
 	
 	private void Update() {
-		if (timedActivationEnabled) {
-			bool timedExtendedState = CalcTimedExtendedState();
-			if (_isExtended != timedExtendedState) {
-				SetExtended(timedExtendedState);
+		if (timedActivationEnabled && !trampleActivationEnabled) {
+			if (_isExtended != CalcTimedExtendedState()) {
+				SetExtended(!_isExtended);
 			}
 		}
 	}
@@ -38,16 +40,32 @@ public class SpikeController : Resettable {
 	public void SetExtended(bool state) {
 		_isExtended = state;
 		_renderer.sprite = _isExtended ? extended : retracted;
-		_collider.enabled = _isExtended;
+		damageCollider.enabled = _isExtended;
+	}
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		if (!_isExtended && trampleActivationEnabled && other.CompareTag("Player")) {
+			StartCoroutine(TrampleExtend());
+		}
+	}
+
+	private IEnumerator TrampleExtend() {
+		yield return new WaitForSeconds(trampleTriggerOffset);
+		SetExtended(true);
+		yield return new WaitForSeconds(trampleTriggerExtendTime);
+		SetExtended(false);
+		
 	}
 
 	private bool CalcTimedExtendedState() {
 		return MathUtil.FloorMod(LevelTime.time - extendOffset, extendTime + retractTime) < extendTime;
 	}
-
+	
 	public override void SaveState() {
+		_savedWasExtended = _isExtended;
 	}
 
 	public override void ResetState() {
+		_isExtended = _savedWasExtended;
 	}
 }

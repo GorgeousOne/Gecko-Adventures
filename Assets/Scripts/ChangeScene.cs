ad3314483724ab5b2664126337f61cd5a03bd9da
IEatSushi <2klaus.4peter@gmail.com>
2022-06-28 02:19:15 +0200

    add breaking platform

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -4,7 +4,8 @@ using UnityEngine.SceneManagement;
 public class ChangeScene : Interactable {
 
     [SerializeField] private string sceneName;
-
+    [SerializeField] private bool triggerOnEnter;
+    
     protected override void OnInteract() {
         string currentSceneName = SceneManager.GetActiveScene().name;
         PlayerPrefs.SetFloat("Player_X_" + currentSceneName, transform.position.x);
@@ -12,7 +13,16 @@ public class ChangeScene : Interactable {
         PlayerPrefs.SetFloat("Player_Z_" + currentSceneName, transform.position.z);
         LevelLoader.Instance.LoadLevel(sceneName);
     }
-
+    
+    protected new void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Player")) {
+            if (triggerOnEnter) {
+                OnInteract();
+            } else {
+                base.OnTriggerEnter2D(other);
+            }
+        }
+    }
     public override void SaveState() {
     }
 
---
+++ Assets/Scripts/Contraptions/UnstableController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class UnstableController : Trigger {
+
+	private bool _isBroken;
+	private bool _wasBroken;
+
+	private Collider2D _collider2D;
+	private SpriteRenderer _renderer;
+	
+	private void Awake() {
+		_collider2D = GetComponent<Collider2D>();
+		_renderer = GetComponent<SpriteRenderer>();
+	}
+
+	private void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.CompareTag("Player")) {
+			Break();
+		}
+	}
+
+	public void Break() {
+		Activate();
+		GetComponentInChildren<ParticleSystem>().Play();
+		_isBroken = true;
+		_collider2D.enabled = false;
+		_renderer.enabled = false;
+	}
+	
+	public override void SaveState() {
+		_wasBroken = _isBroken;
+	}
+
+	public override void ResetState() {
+		_isBroken = _wasBroken;
+		_collider2D.enabled = !_wasBroken;
+		_renderer.enabled = !_wasBroken;
+	}
+}

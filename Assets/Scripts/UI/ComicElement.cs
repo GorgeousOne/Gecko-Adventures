28e9a47ce4e08fb37592c61b32edffdf5e4971ab
Aaron <2klaus.4peter@gmail.com>
2022-07-05 13:08:52 +0200

    WIP create abstract comic elements

---
+++ Assets/Scripts/UI/ComicElement.cs
@@ -0,0 +1,62 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class that represents a comic element that can be clicked through with interact button.
+/// Can have comic elements as children that have to be clicked through before this element disappears.
+/// </summary>
+public abstract class ComicElement : MonoBehaviour {
+
+	protected List<ComicElement> ChildComics;
+	protected int ActiveChildIndex;
+	
+	protected void Awake() {
+		ChildComics = new List<ComicElement>();
+		
+		foreach(Transform child in transform) {
+			ComicElement childComic = child.gameObject.GetComponent<ComicElement>();
+
+			if (childComic != null) {
+				ChildComics.Add(childComic);
+			}
+		}
+	}
+	
+	/// <summary>
+	/// 
+	/// </summary>
+	public void OnInteract() {
+		if (IsSelfActive()) {
+			Interact();
+
+			if (!IsSelfActive()) {
+				ActiveChildIndex = -1;
+				ActivateNextChild();
+			}
+		} else {
+			if (ActiveChildIndex >= ChildComics.Count) {
+				return;
+			}
+			ChildComics[ActiveChildIndex].OnInteract();
+
+			if (!ChildComics[ActiveChildIndex].IsActive()) {
+				ActivateNextChild();
+			}
+		}
+	}
+
+	private void ActivateNextChild() {
+		if (++ActiveChildIndex < ChildComics.Count) {
+			ChildComics[ActiveChildIndex].Activate();
+		}		
+	}
+
+	public bool IsActive() {
+		return IsSelfActive() || ActiveChildIndex < ChildComics.Count;
+	}
+
+	public abstract void Activate();
+	protected abstract void Interact();
+	protected abstract bool IsSelfActive();
+}
---
+++ Assets/Scripts/UI/ComicImage.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ComicImage : ComicElement {
+
+	[SerializeField] [Min(0)] private float fadeInTime = 0.5f;
+	// [SerializeField] [Min(0)] private float fadeOutTime = 0.5f;
+	[SerializeField] private float clickCooldownTime = 1f;
+
+	private Image _image; 
+	private bool _isActive;
+	private float _coolDown;
+
+	private void OnEnable() {
+		_image = GetComponent<Image>();
+	}
+
+	public override void Activate() {
+		gameObject.SetActive(true);
+		_image.color = fadeInTime > 0 ? new Color(0, 0, 0, 0) : Color.white;
+		_isActive = true;
+		_coolDown = clickCooldownTime;
+	}
+	
+	private void Update() {
+		Color color = _image.color;
+
+		if (color.a < 1) {
+			color.a = MathF.Min(1, color.a + (1 / fadeInTime) * Time.deltaTime);
+		}
+		if (_isActive && _coolDown > 0) {
+			_coolDown -= Time.deltaTime;
+		}
+	}
+
+	protected override void Interact() {
+		if (_isActive && _coolDown <= 0) {
+			_isActive = false;
+			gameObject.SetActive(false);
+		}
+	}
+
+	protected override bool IsSelfActive() {
+		return _isActive;
+	}
+}

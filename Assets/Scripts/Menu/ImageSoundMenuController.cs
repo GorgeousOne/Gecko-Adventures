94a9589df97d0d7d9142ecce7abe54f538bd7c16
Aaron <2klaus.4peter@gmail.com>
2022-07-13 00:25:49 +0200

    fix outro visuals glitch

--- Assets/Scripts/Menu/ImageSoundMenuController.cs
+++ Assets/Scripts/Menu/ImageSoundMenuController.cs
@@ -1,36 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ImageSoundMenuController : MonoBehaviour {
 
-    [SerializeField] private GameObject canvas;
+	[SerializeField] private GameObject crashSound;
+	
+	private AudioSource[] _menuAudiosList;
 
-    private GameObject _intro;
-    private GameObject _blackImage;
-    private GameObject _blackImageAudio;
-    private AudioSource[] _listOfMenuAudios;
+	private void Start() {
+		_menuAudiosList = GetComponentsInChildren<AudioSource>(true);
+		// background audio
+		_menuAudiosList[0].enabled = true;
+	}
 
-    private GameObject _otherBlackImage;
-
-    // Start is called before the first frame update
-    void Start() {
-        _listOfMenuAudios = GetComponentsInChildren<AudioSource>(true);
-        // background audio
-        _listOfMenuAudios[0].enabled = true;
-        _intro = canvas.transform.GetChild(3).gameObject;
-        _blackImage = _intro.transform.GetChild(8).gameObject;
-        _blackImageAudio = _blackImage.transform.GetChild(0).gameObject;
-
-        _otherBlackImage = canvas.transform.GetChild(2).gameObject;
-    }
-
-    // Update is called once per frame
-    void Update() {
-        if (_blackImageAudio.activeSelf) {
-            _listOfMenuAudios[0].enabled = false;
-            _otherBlackImage.SetActive(true);
-        }
-
-    }
+	private void Update() {
+		if (crashSound.activeSelf) {
+			_menuAudiosList[0].enabled = false;
+		}
+	}
 }
--- Assets/Scripts/Menu/SoundController.cs
+++ Assets/Scripts/Menu/SoundController.cs
@@ -1,35 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class SoundController : MonoBehaviour{
 
-    [SerializeField] Slider VolumeSlider;
+    [FormerlySerializedAs("VolumeSlider")] [SerializeField] private Slider volumeSlider;
 
     // Start is called before the first frame update
-    void Start() {
+    private void Start() {
         if(!PlayerPrefs.HasKey("volume")) {
             PlayerPrefs.SetFloat("volume", 1);
-            Load();
         }
-        else {
-            Load();
-        }
-        
+        LoadVolumeSetting();
     }
 
     public void ChangeVolume() {
-        AudioListener.volume = VolumeSlider.value;
-        Save();
+        AudioListener.volume = volumeSlider.value;
+        SaveVolumeSetting();
     }
 
-    private void Load() {
-        VolumeSlider.value = PlayerPrefs.GetFloat("volume");
+    private void LoadVolumeSetting() {
+        volumeSlider.value = PlayerPrefs.GetFloat("volume");
     }
 
-    private void Save() {
-        PlayerPrefs.SetFloat("volume", VolumeSlider.value);
+    private void SaveVolumeSetting() {
+        PlayerPrefs.SetFloat("volume", volumeSlider.value);
     }
    
 }

866b8ee2a821f65d763dfab7bfe401b82f69cad8
Aaron <2klaus.4peter@gmail.com>
2022-07-12 13:29:55 +0200

    add comic delay

---
+++ Assets/Scripts/UI/ComicDelay.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class ComicDelay : ComicElement {
+
+	[SerializeField] [Min(0.1f)] private float waitTime = 3f;
+
+	private float _coolDown;
+	
+	public override void Activate(Action deactivateCallback) {
+		base.Activate(deactivateCallback);
+		StartCoroutine(DeactivateTimed());
+	}
+
+	protected override bool IsSelfActive() {
+		return true;
+	}
+	
+	private IEnumerator DeactivateTimed() {
+		yield return new WaitForSeconds(waitTime);
+		Deactivate();
+	}
+}

494d9a748d54fe4b3987a6e0f4ac19bd6bfdde39
IEatSushi <2klaus.4peter@gmail.com>
2025-03-17 23:50:04 +0100

    make tripwire easier to use

--- Assets/Scripts/Contraptions/TripwireController.cs
+++ Assets/Scripts/Contraptions/TripwireController.cs
@@ -1,46 +1,144 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEditor;
 
-public class TripwireController : Trigger, IResettable {
+public class TripwireController : Trigger, IResettable  {
+    public Transform startPoint;
+    public Transform endPoint;
+    public LineRenderer lineRenderer;
+    public EdgeCollider2D edgeCollider;
+    public AudioSource tripSound;
+    private bool _savedIsIntact = true;
+    private bool _isIntact = true;
+    
+    private void Start() {
+       if (startPoint == null || endPoint == null) {
+          Debug.LogError("Start point or end point is not assigned!");
+          return;
+       }
+       // Set the line renderer to have 2 points
+       lineRenderer.positionCount = 2;
+       UpdateLinePositions();
+    }
+    
+    public void UpdateLinePositions() {
+       if (lineRenderer == null || startPoint == null || endPoint == null) {
+          return;
+       }
+       // Update line positions
+       lineRenderer.SetPosition(0, startPoint.position);
+       lineRenderer.SetPosition(1, endPoint.position);
+       
+       // Update edge collider to match line renderer
+       UpdateEdgeCollider();
+    }
+    
+    private void UpdateEdgeCollider() {
+        if (edgeCollider == null || startPoint == null || endPoint == null) {
+            return;
+        }
+        
+        // Convert world positions to local positions for the edge collider
+        Vector2[] points = new Vector2[2];
+        points[0] = transform.InverseTransformPoint(startPoint.position);
+        points[1] = transform.InverseTransformPoint(endPoint.position);
+        
+        // Set the points to the edge collider
+        edgeCollider.points = points;
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other) {
+       if (other.CompareTag("Player")) {
+          Activate();
+          tripSound.Play();
+          edgeCollider.enabled = false;
+          lineRenderer.enabled = false;
+          _isIntact = false;
+       }
+    }
 
-	private LineRenderer _line;
-	private bool _savedIsIntact;
-	
-	private void Start() {
-		_line = GetComponent<LineRenderer>();
-		_CreateCollider();
-		SaveState();
-	}
+    public void SaveState() {
+       _savedIsIntact = _isIntact;
+    }
 
-	private void _CreateCollider() {
-		Vector3[] linePoints = new Vector3[_line.positionCount];
-		_line.GetPositions(linePoints);
-
-		Vector3 start = linePoints[0];
-		Vector3 end = linePoints[1];
-		Vector3 distance = end - start;
-
-		Vector3 colliderOffset = start - transform.position + .5f * distance;
-		float colliderAngle = Mathf.Atan2(distance.y, distance.x) * Mathf.Rad2Deg;
-		
-		BoxCollider2D collider = gameObject.AddComponent<BoxCollider2D>();
-		collider.offset = Quaternion.AngleAxis(-colliderAngle, Vector3.forward) * colliderOffset;
-		collider.size = new Vector2(distance.magnitude, _line.startWidth);
-		collider.isTrigger = true;
-		transform.rotation = Quaternion.Euler(0, 0,  colliderAngle);
-	}
-
-	private void OnTriggerEnter2D(Collider2D other) {
-		if (other.CompareTag("Player")) {
-			Activate();
-			gameObject.SetActive(false);
-		}
-	}
-
-	public void SaveState() {
-		_savedIsIntact = gameObject.activeSelf;
-	}
-
-	public void ResetState() {
-		gameObject.SetActive(_savedIsIntact);
-	}
+    public void ResetState() {
+       if (!_isIntact && _savedIsIntact) {
+          edgeCollider.enabled = true;
+          lineRenderer.enabled = true;
+       }
+       _isIntact = _savedIsIntact;
+    }
 }
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(TripwireController))]
+public class LineEndpointsEditor : Editor {
+    private TripwireController tripwireController;
+    private Transform selectedPoint;
+    private Vector3 lastParentPosition;
+    private Quaternion lastParentRotation;
+
+    private void OnEnable() {
+       tripwireController = (TripwireController) target;
+       if (tripwireController != null) {
+          lastParentPosition = tripwireController.transform.position;
+          lastParentRotation = tripwireController.transform.rotation;
+          // Ensure SceneView repaints on selection
+          SceneView.duringSceneGui += OnSceneGUIDelegate;
+       }
+    }
+
+    private void OnDisable() {
+       SceneView.duringSceneGui -= OnSceneGUIDelegate;
+    }
+
+    private void OnSceneGUIDelegate(SceneView sceneView) {
+       // This will be called every frame in the Scene view
+       if (tripwireController != null) {
+          // Check if parent transform has changed
+          if (tripwireController.transform.position != lastParentPosition ||
+              tripwireController.transform.rotation != lastParentRotation) {
+             lastParentPosition = tripwireController.transform.position;
+             lastParentRotation = tripwireController.transform.rotation;
+             tripwireController.UpdateLinePositions();
+             SceneView.RepaintAll();
+          }
+       }
+    }
+
+    public override void OnInspectorGUI() {
+       DrawDefaultInspector();
+    }
+
+    private void OnSceneGUI() {
+       if (tripwireController == null || tripwireController.startPoint == null ||
+           tripwireController.endPoint == null)
+          return;
+
+       // Draw handles for start and end points
+       EditorGUI.BeginChangeCheck();
+       Vector3 newStartPos = Handles.PositionHandle(tripwireController.startPoint.position, Quaternion.identity);
+       if (EditorGUI.EndChangeCheck()) {
+          Undo.RecordObject(tripwireController.startPoint, "Move Start Point");
+          tripwireController.startPoint.position = newStartPos;
+          tripwireController.UpdateLinePositions();
+          EditorUtility.SetDirty(tripwireController.startPoint);
+       }
+
+       EditorGUI.BeginChangeCheck();
+       Vector3 newEndPos = Handles.PositionHandle(tripwireController.endPoint.position, Quaternion.identity);
+       if (EditorGUI.EndChangeCheck()) {
+          Undo.RecordObject(tripwireController.endPoint, "Move End Point");
+          tripwireController.endPoint.position = newEndPos;
+          tripwireController.UpdateLinePositions();
+          EditorUtility.SetDirty(tripwireController.endPoint);
+       }
+
+       // Force update when parent transform changes
+       if (Event.current.type == EventType.MouseUp) {
+          lastParentPosition = tripwireController.transform.position;
+          lastParentRotation = tripwireController.transform.rotation;
+          tripwireController.UpdateLinePositions();
+       }
+    }
+}
+#endif
